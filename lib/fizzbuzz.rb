a19890b9451a756b786dfbd006a9002685dbd094
Benzoh.Wada <benzoh.wada@gmail.com>
2018-02-21 08:24:45 +0900

    - 修正・ディレクトリ構成変更

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(count)
-    if (count % 3 == 0) && (count % 5 == 0)
+    if count % 15 == 0
         "FizzBuzz"
-    elsif (count % 3) == 0
+    elsif count % 3 == 0
         "Fizz"
     elsif count % 5 == 0
         "Buzz"
