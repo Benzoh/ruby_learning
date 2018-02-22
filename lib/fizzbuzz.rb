a3a3a4077275bd93f2634faa5084edebbdaad32d
Benzoh.Wada <benzoh.wada@gmail.com>
2018-02-23 08:07:59 +0900

    インデントはスペース２がベターなんだってさ

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,15 +1,15 @@
 def fizzbuzz(count)
-    if count % 15 == 0
-        "FizzBuzz"
-    elsif count % 3 == 0
-        "Fizz"
-    elsif count % 5 == 0
-        "Buzz"
-    else
-        count.to_s
-    end
+  if count % 15 == 0
+    "FizzBuzz"
+  elsif count % 3 == 0
+    "Fizz"
+  elsif count % 5 == 0
+    "Buzz"
+  else
+    count.to_s
+  end
 end
 
-(1...100).each do |i|
-    p fizzbuzz(i)
+(1...100).each do |i |
+  p fizzbuzz(i)
 end
